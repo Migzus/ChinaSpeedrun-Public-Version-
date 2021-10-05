4751f6f106d6690f7a2acb22fab834131e4fcac0
Jmannorway <synthas@protonmail.com>
2021-10-05 16:33:20 +0200

    Merge push

--- ChinaSpeedrun/World.cpp
+++ ChinaSpeedrun/World.cpp
@@ -1,48 +1,9 @@
 #include "World.h"
 
-#include <iostream>
-
-#include "CameraComponent.h"
-#include "CameraSystem.h"
-#include "TransformComponent.h"
-#include "MovementComponent.h"
-#include "MovementSystem.h"
-
 World::World() {
-	/*entt::entity _entity = registry.create();
-	registry.emplace<CameraComponent>(_entity);
-	registry.emplace<TransformComponent>(_entity);
-	registry.emplace<MovementComponent>(_entity);*/
+
 }
 
-void World::Step()
-{
-	/*
-	auto _group = registry.group<MovementComponent>(entt::get<TransformComponent>);
-	for (auto _entity : _group) {
-		auto& [_movement, _transform] = _group.get<MovementComponent, TransformComponent>(_entity);
-		MovementSystem::HandleMovement(_movement, _transform);
-	}
+void World::Step() {
 	
-	for (int i = 0; i < 10; i++) {
-		registry.emplace<Matrix4x4>(registry.create());
-	}
-
-	auto _entity = registry.create();
-	registry.emplace<CameraComponent>(_entity);
-
-	for (int i = 0; i < 10; i++) {
-		registry.emplace<Matrix4x4>(registry.create());
-	}
-
-	registry.sort<CameraComponent, Matrix4x4>();
-
-	entt::basic_view view = registry.view<CameraComponent>();
-
-	CameraSystem system;
-
-	for (auto& e : view) {
-		const auto& camera = view.get<CameraComponent>(e);
-
-	}*/
 }
